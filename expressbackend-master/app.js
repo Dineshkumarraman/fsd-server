3377d9d806e442e549f5e7a276088fa29e8dbb7d
Dineshkumarraman <31721668+Dineshkumarraman@users.noreply.github.com>
2017-09-21 15:48:20 +0530

    Changing port number

--- expressbackend-master/app.js
+++ expressbackend-master/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var port = 1341;
+var port = 4200;
 var cors = require('cors');
 
 // Mongoose connection with mongodb
